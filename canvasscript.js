9da97bf822ad868221fc2d4d0f7d066fd2df44d5
Amal <amal1293@gmail.com>
2013-08-03 14:21:57 +0530

    First COmmit

--- canvasscript.js
+++ canvasscript.js
@@ -8,6 +8,15 @@ var initInterv;
 var mouseX,mouseY;
 var mouseMove = 0;
 
+var grantGraceTime = 0;
+var graceTime = 0;
+
+var lives = 3;
+
+var out = 0;
+
+var mousePointColor = '#438ad0';
+
 startInitLoad();
 
 function startInitLoad(){
@@ -17,6 +26,15 @@ var num = 0;
 
 
 function initLoad(){
+	if(grantGraceTime ==1){
+		graceTime++;
+		mousePointColor = 'yellow';
+		if(graceTime == 200){
+			graceTime = 0;
+			grantGraceTime = 0;
+			mousePointColor = '#438ad0';
+		}
+	}
 	//console.log(num);
 	context.fillStyle='black';
 	context.fillRect(0,0,600,600);
@@ -27,8 +45,8 @@ function initLoad(){
 	for(var i=0;i<genNow/2;i++){
 		//console.log("adasD");
 		
-		var radius = 3+Math.floor(Math.random()*3);
-		console.log(radius);
+		var radius = 3+(Math.random()*3);
+		//console.log(radius);
 		var angle = 210;
 		angleRad = (Math.PI*angle)/180;
 		var speed = 1+Math.floor(Math.random()*4);
@@ -42,15 +60,14 @@ function initLoad(){
 		//console.log(dots[dots.length-1][0]+" "+dots[dots.length-1][1]+" "+dots[dots.length-1][2]+" "+dots[dots.length-1][3]+" "+dots[dots.length-1][4]);
 		context.fillStyle= 'red';
 		context.beginPath();
-		context.arc(600,startLoc,radius,0,2*Math.PI);
+		context.arc(604,startLoc,radius,0,2*Math.PI);
 		context.closePath();
 		context.fill();
-		
 		}
 		
 	}
 	num++;
-		if(num==10)
+		if(num==30)
 			num=0;
 	
 	/*for(var i=0;i<genNow/2;i++){
@@ -78,13 +95,14 @@ function initLoad(){
 		if(typeof(dots[j]) != 'undefined'){
 		if(dots[j][0] <= 0 || dots[j][1] >= 600){
 			//console.log("asdasDSA");
-			dots.splice(j,1);
+			delete dots[j];
+			//alert('deleted');
 		}
 		// Deciding Position
 		
 		
 		//console.log(dots[j][4]);
-		if(typeof(dots[j][4] != 'undefined')){
+		if(typeof(dots[j][4]) != 'undefined'){
 		var moveXBy = dots[j][4]*Math.cos(angleRad);
 		var moveYBy = dots[j][4]*Math.sin(angleRad);
 		//console.log(distanceToTravel);
@@ -99,14 +117,71 @@ function initLoad(){
 		context.closePath();
 		context.fill();
 	}
+	if(mouseMove == 1){
+		if(mouseX >= dots[j][0]-dots[j][2] && mouseX <= dots[j][0]+dots[j][2] && mouseY >= dots[j][1]-dots[j][2] && mouseY <= dots[j][1]+dots[j][2]){
+			
+
+			if(grantGraceTime == 0){
+				lives--;
+				grantGraceTime = 1;
+			}
+			if(lives == 0)
+				alert("Out");
+		}
+	}
+/*	if(mouseMove == 1 && mouseX-dots[j][0] ){
+		//console.log("sad");
+		//console.log(dots[j][2]);
+		//if(mouseX >= dots[j][0]-dots[j][2] && mouseX <= dots[j][0]+dots[j][2] && mouseY >= dots[j][0]-dots[j][2] && mouseY <= dots[j][0]+dots[j][2])
+			//alert("Out");
+	}*/
 	}
 }
 	if(mouseMove == 1){
-		context.fillStyle = '#438ad0';
+		context.fillStyle = mousePointColor;
 		context.beginPath();
-		context.arc(mouseX,mouseY,2,0,2*Math.PI);
+		context.arc(mouseX,mouseY,3,0,2*Math.PI);
 		context.closePath();
 		context.fill();
+
+		context.strokeStyle = mousePointColor;
+		context.beginPath();
+		context.lineWidth = 1;
+		context.moveTo(mouseX,mouseY);
+		context.lineTo(mouseX+(10/Math.cos(angleRad)),mouseY-(10/Math.sin(angleRad)))
+		context.stroke();
+
+		if(lives>=2){
+		context.fillStyle = mousePointColor;
+		context.beginPath();
+		context.arc(mouseX+(10/Math.cos(angleRad)),mouseY-(10/Math.sin(angleRad)),3,0,2*Math.PI);
+		context.closePath();
+		context.fill();
+	}
+
+
+		context.strokeStyle = mousePointColor;
+		context.beginPath();
+		context.lineWidth = 1;
+		context.moveTo(mouseX,mouseY);
+		context.lineTo(mouseX+(20/Math.cos(angleRad)),mouseY-(20/Math.sin(angleRad)))
+		context.stroke();
+
+		if(lives>=3){
+		context.fillStyle = mousePointColor;
+		context.beginPath();
+		context.arc(mouseX+(20/Math.cos(angleRad)),mouseY-(20/Math.sin(angleRad)),3,0,2*Math.PI);
+		context.closePath();
+		context.fill();
+	}
+
+		context.strokeStyle = mousePointColor;
+		context.beginPath();
+		context.lineWidth = 1;
+		context.moveTo(mouseX,mouseY);
+		context.lineTo(mouseX+(30/Math.cos(angleRad)),mouseY-(30/Math.sin(angleRad)))
+		context.stroke();
+
 	}
 }
 
@@ -116,4 +191,12 @@ function generateThread(e){
 
 	mouseMove = 1;
 
+}
+
+function gameOut(e){
+	if(out == 0){
+	alert("Out");
+	out = 1;
+	clearInterval(initInterv);
+}
 }
